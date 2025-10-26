0583b6640c566a51aa28c21c7f1ad75f6d1f9696
Iliass Lasri <iliass.lasri@outlook.com>
2025-10-26 15:09:41 +0100

    moved to hydra

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 # train.py
 import re
-import time
 from pathlib import Path
 
 import pandas as pd
@@ -19,22 +18,19 @@ from pathlib import Path
 import soundfile as sf
 from datetime import datetime
 from torch.optim.lr_scheduler import CosineAnnealingLR
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-NUM_SAMPLES = 1024
-N_EPOCHS = int(10e5)
+
 BASE_DIR = Path(__file__).resolve().parent
-SAMPLE_RATE = 24_000
 
-# RVQ
-N_BINS = 1024
-N_QUANTIZERS = 32
-BATCH_SZ = 1
-
-def main():
+@hydra.main(config_path="configs", config_name="train")
+def main(cfg: DictConfig = None):
+    print(OmegaConf.to_yaml(cfg))
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    writer = SummaryWriter(log_dir=f"runs/tts_{timestamp}")
-    checkpoint_dir = Path(f"runs/tts_{timestamp}/checkpoints")
+    writer = SummaryWriter(log_dir=f"{cfg.logging.log_dir}/tts_{timestamp}")
+    checkpoint_dir = Path(f"{cfg.logging.log_dir}/tts_{timestamp}/checkpoints")
     checkpoint_dir.mkdir(parents=True, exist_ok=True)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -46,9 +42,8 @@ def main():
     encodec_model.to(device)
 
     # Dataset & DataLoader
-    root = Path("./datasets/cmu_us_awb_arctic")
-    wav_dir = root / "wav"
-    text_file = root / "etc" / "txt.done.data"
+    wav_dir = BASE_DIR / Path(cfg.dataset.root_dir) / cfg.dataset.wav_dir
+    text_file = BASE_DIR / Path(cfg.dataset.root_dir) / cfg.dataset.text_file
 
     # Parse the transcripts
     pattern = re.compile(r'^\(\s*(\S+)\s+"(.+)"\s*\)$')
@@ -71,43 +66,45 @@ def main():
     print(f"Valid audio files: {len(df)} remaining, after filtering with os.path.exists")
     print("Head of dataFrame \n")
     print(df.head())
-    dataset = LJSpeechDataset(df, tokenizer, encodec_model, sample_rate=SAMPLE_RATE, num_samples=NUM_SAMPLES)
-    dataloader = DataLoader(dataset, batch_size=BATCH_SZ, shuffle=True, collate_fn=collate_fn)
+    dataset = LJSpeechDataset(df, tokenizer, encodec_model, sample_rate=cfg.train.sample_rate, num_samples=cfg.train.num_samples)
+    dataloader = DataLoader(dataset, batch_size=cfg.train.batch_size, shuffle=True, collate_fn=collate_fn)
     
     # Model
     text_vocab_size = tokenizer.vocab_size
-    text_embed_dim = 512
-    text_num_layers = 4
 
     model = TTSModel(
-        text_vocab_size, text_embed_dim, text_num_layers,
-        N_BINS, N_QUANTIZERS, rvq_embed_dim=text_embed_dim, 
-        num_decoder_layers=3, #6 worked in overfit
+        text_vocab_size, 
+        text_embed_dim=cfg.model.text_embed_dim,
+        text_num_layers=cfg.model.text_num_layers,
+        encodec_codebook_size=cfg.rvq.n_bins,
+        encodec_num_quantizers=cfg.rvq.n_quantizers,
+        rvq_embed_dim=cfg.model.text_embed_dim,
+        num_decoder_layers=cfg.model.num_decoder_layers
     )
     model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=cfg.train.learning_rate)
 
     # lr tuning
-    scheduler = CosineAnnealingLR(optimizer, T_max=N_EPOCHS, eta_min=1e-6)
+    scheduler = CosineAnnealingLR(optimizer, T_max=cfg.train.n_epochs, eta_min=1e-6)
 
     print("-"*100)
     print("SUMMARY OF MODEL")
     print("-"*100)
     token_ids_example = torch.randint(0, tokenizer.vocab_size, (1, 11), dtype=torch.long)
-    rvq_token_ids_example = torch.randint(0, N_BINS, (1, 300, N_QUANTIZERS), dtype=torch.long)
+    rvq_token_ids_example = torch.randint(0, cfg.rvq.n_bins, (1, 300, cfg.rvq.n_quantizers), dtype=torch.long)
 
     # Move to device if needed
     token_ids_example = token_ids_example.to(device)
     rvq_token_ids_example = rvq_token_ids_example.to(device)
+    accumulation_steps = cfg.train.accumulation_steps
 
     print(model)
     print("Total params:", sum(p.numel() for p in model.parameters()))
     print("Trainable params:", sum(p.numel() for p in model.parameters() if p.requires_grad))
     print("-"*100)
     print("-"*100)
-    accumulation_steps = 32
     # Training loop
-    for epoch in range(N_EPOCHS):
+    for epoch in range(cfg.train.n_epochs):
         model.train()
         total_loss = 0
         batch_idx = 0
@@ -124,8 +121,7 @@ def main():
             rvq_targets = torch.stack(encodec_batch, dim=-1)  # [B, L, N_QUANTIZERS]
 
             # For teacher forcing, shift right and add a <BOS> token 
-            bos_token = 0
-            rvq_input = torch.full((rvq_targets.shape[0], 1, N_QUANTIZERS), bos_token, dtype=torch.long, device=device)
+            rvq_input = torch.full((rvq_targets.shape[0], 1, cfg.rvq.n_quantizers), cfg.train.bos_token, dtype=torch.long, device=device)
 
             # Concatenate BOS with all tokens except last
             rvq_token_ids = torch.cat([rvq_input, rvq_targets[:, :-1, :]], dim=1)  # [B, L, N_QUANTIZERS]
@@ -142,7 +138,7 @@ def main():
             min_len = min(L_pred, L_target)
             pred = pred[:, :min_len, :, :]
             target_stack = target_stack[..., :min_len, :]
-            target_onehot = F.one_hot(target_stack, num_classes=N_BINS).float()
+            target_onehot = F.one_hot(target_stack, num_classes=cfg.rvq.n_bins).float()
             loss = F.cross_entropy(pred, target_onehot)
             # Only update weights every accumulation_steps
             # Scale the loss
@@ -156,38 +152,39 @@ def main():
             print("gloabl step", global_step, "loss=", loss.item())
             if (global_step + 1) % accumulation_steps == 0:
                 optimizer.step()
-                scheduler.step()     # if you use a scheduler
-                optimizer.zero_grad()
+                scheduler.step()
+                optimizer.zero_grad(set_to_none=True)
             # optimizer.step()
             # scheduler.step()
             total_loss += loss.item()
 
             # reconstruct audio from predicted tokens for monitoring
-            if global_step % 5000 == 0:
+            if global_step % cfg.logging.save_every == 0:
                 with torch.no_grad():
                     # take argmax as predicted tokens for now (TODO)
                     pred_tokens = torch.argmax(logits, dim=-1)
                     # pred_tokens: list of [B, L] per quantizer
-                    pred_list = [pred_tokens[..., i] for i in range(N_QUANTIZERS)]
+                    pred_list = [pred_tokens[..., i] for i in range(cfg.rvq.n_quantizers)]
                     pred_tokens_tensor = torch.stack(pred_list, dim=1).to('cpu')
                     encoded_frames = [(pred_tokens_tensor, None)]
                     encodec_model_cpu = encodec_model.to('cpu')
                     reconstructed = encodec_model_cpu.decode(encoded_frames)
                     # reconstructed: [B, 1, T]
-                    sf.write(f"runs/tts_{timestamp}/reconstructed_epoch{epoch}.wav", reconstructed[0][0].cpu().numpy(), SAMPLE_RATE)
+                    sf.write(f"runs/tts_{timestamp}/reconstructed_epoch{epoch}.wav", reconstructed[0][0].cpu().numpy(), cfg.train.sample_rate)
 
                     gt_tokens = torch.stack(encodec_batch, dim=1).to('cpu')  # [B, N_Q, L]
                     encoded_frames_gt = [(gt_tokens, None)]
                     reconstructed_gt = encodec_model_cpu.decode(encoded_frames_gt)  # [B, 1, T]
-                    sf.write(f"runs/tts_{timestamp}/original_epoch{epoch}.wav", reconstructed_gt[0][0].cpu().numpy(), SAMPLE_RATE)
+                    sf.write(f"runs/tts_{timestamp}/original_epoch{epoch}.wav", reconstructed_gt[0][0].cpu().numpy(), cfg.train.sample_rate)
 
                     torch.save(model.state_dict(), checkpoint_dir / f"model_epoch_{epoch}.pt")
                     print(f"[INFO] Saved checkpoint: model_epoch_{epoch}.pt")
                     encodec_model.to(device)
-            del logits
+            del logits, encodec_batch, token_ids
     final_model_path = checkpoint_dir / "model_final.pt"
     torch.save(model.state_dict(), final_model_path)
     print(f"[INFO] Final model saved to {final_model_path}")
     writer.close()
+
 if __name__ == "__main__":
     main()
