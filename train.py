811bf8a8d55abc9fb8c61978b4942f2890959759
Iliass Lasri <iliass.lasri@outlook.com>
2025-11-03 18:00:53 +0100

    adam -> adamW

--- train.py
+++ train.py
@@ -20,6 +20,7 @@ from model.tts_model import TTSModel
 
 BASE_DIR = Path(__file__).resolve().parent
 
+
 @hydra.main(config_path="configs", config_name="train")
 def main(cfg: DictConfig = None):
     print(OmegaConf.to_yaml(cfg))
@@ -45,49 +46,74 @@ def main(cfg: DictConfig = None):
     pattern = re.compile(r'^\(\s*(\S+)\s+"(.+)"\s*\)$')
     rows = []
     english_normalizer = EnglishTextNormalizer()
-    
+
     with open(text_file, "r") as f:
         for line in f:
             match = pattern.search(line)
             if match:
                 file_id, text = match.groups()
                 import os
+
                 wav_path = wav_dir / f"{file_id}.wav"
                 wav_path = os.path.abspath(os.path.join(BASE_DIR, wav_path))
-                rows.append({"file_id": file_id, "text": english_normalizer(text), "path": str(wav_path)})
+                rows.append(
+                    {
+                        "file_id": file_id,
+                        "text": english_normalizer(text),
+                        "path": str(wav_path),
+                    }
+                )
 
     df = pd.DataFrame(rows)
     print(f"All audio files: {len(df)}")
     df = df[df["path"].apply(os.path.exists)].reset_index(drop=True)
-    print(f"Valid audio files: {len(df)} remaining, after filtering with os.path.exists")
+    print(
+        f"Valid audio files: {len(df)} remaining, after filtering with os.path.exists"
+    )
     print("Head of dataFrame \n")
     print(df.head())
-    dataset = LJSpeechDataset(df, tokenizer, encodec_model, sample_rate=cfg.train.sample_rate, num_samples=cfg.train.num_samples)
-    dataloader = DataLoader(dataset, batch_size=cfg.train.batch_size, shuffle=True, collate_fn=collate_fn)
-    
+    dataset = LJSpeechDataset(
+        df,
+        tokenizer,
+        encodec_model,
+        sample_rate=cfg.train.sample_rate,
+        num_samples=cfg.train.num_samples,
+    )
+    dataloader = DataLoader(
+        dataset, batch_size=cfg.train.batch_size, shuffle=True, collate_fn=collate_fn
+    )
+
     # Model
     text_vocab_size = tokenizer.vocab_size
 
     model = TTSModel(
-        text_vocab_size, 
+        text_vocab_size,
         text_embed_dim=cfg.model.text_embed_dim,
         text_num_layers=cfg.model.text_num_layers,
         encodec_codebook_size=cfg.rvq.n_bins,
         encodec_num_quantizers=cfg.rvq.n_quantizers,
         rvq_embed_dim=cfg.model.text_embed_dim,
-        num_decoder_layers=cfg.model.num_decoder_layers
+        num_decoder_layers=cfg.model.num_decoder_layers,
     )
     model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=cfg.train.learning_rate)
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=cfg.train.learning_rate,
+        weight_decay=cfg.train.weight_decay,
+    )
 
     # lr tuning
     scheduler = CosineAnnealingLR(optimizer, T_max=cfg.train.n_epochs, eta_min=1e-6)
 
-    print("-"*100)
+    print("-" * 100)
     print("SUMMARY OF MODEL")
-    print("-"*100)
-    token_ids_example = torch.randint(0, tokenizer.vocab_size, (1, 11), dtype=torch.long)
-    rvq_token_ids_example = torch.randint(0, cfg.rvq.n_bins, (1, 300, cfg.rvq.n_quantizers), dtype=torch.long)
+    print("-" * 100)
+    token_ids_example = torch.randint(
+        0, tokenizer.vocab_size, (1, 11), dtype=torch.long
+    )
+    rvq_token_ids_example = torch.randint(
+        0, cfg.rvq.n_bins, (1, 300, cfg.rvq.n_quantizers), dtype=torch.long
+    )
 
     # Move to device if needed
     token_ids_example = token_ids_example.to(device)
@@ -96,9 +122,12 @@ def main(cfg: DictConfig = None):
 
     print(model)
     print("Total params:", sum(p.numel() for p in model.parameters()))
-    print("Trainable params:", sum(p.numel() for p in model.parameters() if p.requires_grad))
-    print("-"*100)
-    print("-"*100)
+    print(
+        "Trainable params:",
+        sum(p.numel() for p in model.parameters() if p.requires_grad),
+    )
+    print("-" * 100)
+    print("-" * 100)
     # Training loop
     for epoch in range(cfg.train.n_epochs):
         model.train()
@@ -106,36 +135,47 @@ def main(cfg: DictConfig = None):
         batch_idx = 0
         for token_ids, encodec_batch in dataloader:
             global_step = epoch * len(dataloader) + batch_idx
-            batch_idx += 1 
+            batch_idx += 1
             token_ids = token_ids.to(device)
             encodec_batch = [q.to(device) for q in encodec_batch]
 
             optimizer.zero_grad()
-            assert token_ids.max() < tokenizer.vocab_size, "Token ID exceeds vocab size!"
+            assert (
+                token_ids.max() < tokenizer.vocab_size
+            ), "Token ID exceeds vocab size!"
             # encodec_batch: list of [B, L] per quantizer, length = N_QUANTIZERS
             # Stack along last dimension: [B, L, N_QUANTIZERS]
             rvq_targets = torch.stack(encodec_batch, dim=-1)  # [B, L, N_QUANTIZERS]
 
-            # For teacher forcing, shift right and add a <BOS> token 
-            rvq_input = torch.full((rvq_targets.shape[0], 1, cfg.rvq.n_quantizers), cfg.train.bos_token, dtype=torch.long, device=device)
+            # For teacher forcing, shift right and add a <BOS> token
+            rvq_input = torch.full(
+                (rvq_targets.shape[0], 1, cfg.rvq.n_quantizers),
+                cfg.train.bos_token,
+                dtype=torch.long,
+                device=device,
+            )
 
             # Concatenate BOS with all tokens except last
-            rvq_token_ids = torch.cat([rvq_input, rvq_targets[:, :-1, :]], dim=1)  # [B, L, N_QUANTIZERS]
-            
-            logits = model(token_ids, rvq_token_ids)  # list of [B, L, codebook_size] per quantizer
+            rvq_token_ids = torch.cat(
+                [rvq_input, rvq_targets[:, :-1, :]], dim=1
+            )  # [B, L, N_QUANTIZERS]
+
+            logits = model(
+                token_ids, rvq_token_ids
+            )  # list of [B, L, codebook_size] per quantizer
 
             # compute cross-entropy for each quantizer
             pred = logits
             target = encodec_batch
 
             # Ensure seq lengths match
-            target_stack = torch.stack(target, dim=2) 
+            target_stack = torch.stack(target, dim=2)
             min_len = min(pred.shape[1], target_stack.shape[-2])
             pred = pred[:, :min_len, :, :]
             target_stack = target_stack[..., :min_len, :]
-            logits = pred.permute(0, 3, 1, 2) # [B, L, Q, N_BINS] -> [B, N_BINS, L, Q]
+            logits = pred.permute(0, 3, 1, 2)  # [B, L, Q, N_BINS] -> [B, N_BINS, L, Q]
             loss = F.cross_entropy(logits, target_stack)
-            
+
             # Only update weights every accumulation_steps
             # Scale the loss
             loss = loss / accumulation_steps
@@ -160,20 +200,36 @@ def main(cfg: DictConfig = None):
                     # take argmax as predicted tokens for now (TODO)
                     pred_tokens = torch.argmax(logits, dim=-1)
                     # pred_tokens: list of [B, L] per quantizer
-                    pred_list = [pred_tokens[..., i] for i in range(cfg.rvq.n_quantizers)]
-                    pred_tokens_tensor = torch.stack(pred_list, dim=1).to('cpu')
+                    pred_list = [
+                        pred_tokens[..., i] for i in range(cfg.rvq.n_quantizers)
+                    ]
+                    pred_tokens_tensor = torch.stack(pred_list, dim=1).to("cpu")
                     encoded_frames = [(pred_tokens_tensor, None)]
-                    encodec_model_cpu = encodec_model.to('cpu')
+                    encodec_model_cpu = encodec_model.to("cpu")
                     reconstructed = encodec_model_cpu.decode(encoded_frames)
                     # reconstructed: [B, 1, T]
-                    sf.write(f"runs/tts_{timestamp}/reconstructed_epoch{epoch}.wav", reconstructed[0][0].cpu().numpy(), cfg.train.sample_rate)
+                    sf.write(
+                        f"runs/tts_{timestamp}/reconstructed_epoch{epoch}.wav",
+                        reconstructed[0][0].cpu().numpy(),
+                        cfg.train.sample_rate,
+                    )
 
-                    gt_tokens = torch.stack(encodec_batch, dim=1).to('cpu')  # [B, N_Q, L]
+                    gt_tokens = torch.stack(encodec_batch, dim=1).to(
+                        "cpu"
+                    )  # [B, N_Q, L]
                     encoded_frames_gt = [(gt_tokens, None)]
-                    reconstructed_gt = encodec_model_cpu.decode(encoded_frames_gt)  # [B, 1, T]
-                    sf.write(f"runs/tts_{timestamp}/original_epoch{epoch}.wav", reconstructed_gt[0][0].cpu().numpy(), cfg.train.sample_rate)
+                    reconstructed_gt = encodec_model_cpu.decode(
+                        encoded_frames_gt
+                    )  # [B, 1, T]
+                    sf.write(
+                        f"runs/tts_{timestamp}/original_epoch{epoch}.wav",
+                        reconstructed_gt[0][0].cpu().numpy(),
+                        cfg.train.sample_rate,
+                    )
 
-                    torch.save(model.state_dict(), checkpoint_dir / f"model_epoch_{epoch}.pt")
+                    torch.save(
+                        model.state_dict(), checkpoint_dir / f"model_epoch_{epoch}.pt"
+                    )
                     print(f"[INFO] Saved checkpoint: model_epoch_{epoch}.pt")
                     encodec_model.to(device)
             del logits, encodec_batch, token_ids
@@ -181,6 +237,7 @@ def main(cfg: DictConfig = None):
     torch.save(model.state_dict(), final_model_path)
     print(f"[INFO] Final model saved to {final_model_path}")
     writer.close()
+
 
 if __name__ == "__main__":
     main()
